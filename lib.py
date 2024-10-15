9c5aa8c80e2ebbd0764b928e49ea69643aa2f080
Matteo Cencini <matteo.cencini@gmail.com>
2024-10-15 14:07:26 +0200

    change default command name

--- lib.py
+++ lib.py
@@ -19,12 +19,12 @@ DEFAULT_CONFIG = {
     "CHECK_INTERVAL": 2,
     "REMOTE_SERVER_USER": "sdc",
     "REMOTE_SERVER_HOST": "remote-machine-name",
-    "SERVER_COMMAND": "python /srv/nfs/psd/usr/psd/pulseq/bin/external_server.py",
+    "SERVER_COMMAND": "python /srv/nfs/psd/usr/psd/pulseq/v7/bin/external_server.py",
     "SERVER_PROCESS_NAME": "external_server.py",
     "file_path_simulation": "params.dat",
-    "file_path_production": "/srv/nfs/psd/usr/psd/pulseq/temp/params.dat",
+    "file_path_production": "/srv/nfs/psd/usr/psd/pulseq/v7/temp/params.dat",
     "output_path_simulation": "sequence.bin",
-    "output_path_production": "/srv/nfs/psd/usr/psd/pulseq/temp/sequence.bin",
+    "output_path_production": "/srv/nfs/psd/usr/psd/pulseq/v7/temp/sequence.bin",
 }
 
 # Define the default config file location
